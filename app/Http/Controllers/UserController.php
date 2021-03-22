a0aaa5dedad750c6e1cc7c3be4b359273f50ceab
Bishnupkl <bishnup212@gmail.com>
2021-03-22 16:57:00 +0545

    installing passport in laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator=Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required',
+
+    ]);
+
+        if ($validator->fails()) {
+            return response()->json(["val error"=> $validator->errors()]);
+        }
+
+        $data = $request->all();
+        $user = User::create($data);
+
+        if ($user) {
+            return response()->json(["message" => "user created succesfully",
+            ]);
+//            return  response()->json([])
+        }else{
+            return  response()->json(["message" => "user couldnt registered",]);
+
+
+        }
+
+    }
+
+    public function login(Request  $request)
+    {
+        $validator=Validator::make($request->all(), [
+
+            'email' => 'required|email',
+            'password' => 'required',
+
+        ]);
+        if ($validator->fails()) {
+            return response()->json(["val error"=> $validator->errors()]);
+        }
+
+
+    }
+}
